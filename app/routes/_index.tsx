18c13d74cb70927f03542cde38af4dbc5fe9b421
Michael Wu <mw10013@gmail.com>
2023-12-12 16:33:31 -0500

    serverNodeBuiltinsPolyfill

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+// @ts-expect-error ts(7016)
+import * as base32 from "thirty-two";
+import { Buffer } from "node:buffer";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,10 +11,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function loader() {
+  const buf = Buffer.from("hello world", "utf8");
+  return {
+    encode: base32.encode("node").toString(),
+    decode: base32.decode("NZXWIZI=").toString(),
+    buf: buf.toString(),
+    buf64: buf.toString("base64"),
+  };
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix Sandbox Pages</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
