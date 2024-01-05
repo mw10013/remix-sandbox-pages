ef094fd028506094e2e58244e254bd2116ab3a36
Michael Wu <mw10013@gmail.com>
2024-01-05 12:53:59 -0500

    add fee fi

--- app/root.tsx
+++ app/root.tsx
@@ -4,21 +4,56 @@ import {
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useNavigate,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
-// import { RouterProvider } from "react-aria-components";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   { rel: "stylesheet", href: styles },
 ];
 
+function NavBar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Sandbox</a>
+      </div>
+      <div className="navbar-center">
+        <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+          <li>
+            <NavLink to="/fee">Fee</NavLink>
+          </li>
+          <li>
+            <NavLink to="/fi">Fi</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export default function App() {
-  const navigate = useNavigate();
   return (
     <html lang="en">
       <head>
@@ -28,9 +63,8 @@ export default function App() {
         <Links />
       </head>
       <body>
-        {/* <RouterProvider navigate={navigate}> */}
+        <NavBar />
         <Outlet />
-        {/* </RouterProvider> */}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { NavLink } from "@remix-run/react";
 import {
   Button,
   Label,
@@ -18,70 +19,33 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <a className="btn btn-ghost text-xl">Sandbox</a>
+    <div className="container mx-auto py-4 prose-">
+      <h1 className="">Remix Sandbox Pages</h1>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2 flex-wrap">
+          <button className="btn">Button</button>
+          <Button className="btn">Button</Button>
+          <Button className="btn btn-primary">Button</Button>
+          <Button className="btn btn-neutral">Button</Button>
+          <Button className="btn btn-primary">Button</Button>
+          <Button className="btn btn-secondary btn-outline">Button</Button>
+          <Button className="btn btn-ghost">Button</Button>
+          <Button className="btn btn-link">Button</Button>
+          <Button className="btn btn-primary" isDisabled>
+            Disabled
+          </Button>
         </div>
-        <div className="navbar-center">
-          <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li className="active">
-              <a>Item 2</a>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      <div className="container mx-auto py-4 prose-">
-        <h1 className="">Remix Sandbox Pages</h1>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-2 flex-wrap">
-            <button className="btn">Button</button>
-            <Button className="btn">Button</Button>
-            <Button className="btn btn-primary">Button</Button>
-            <Button className="btn btn-neutral">Button</Button>
-            <Button className="btn btn-primary">Button</Button>
-            <Button className="btn btn-secondary btn-outline">Button</Button>
-            <Button className="btn btn-ghost">Button</Button>
-            <Button className="btn btn-link">Button</Button>
-            <Button className="btn btn-primary" isDisabled>
-              Disabled
-            </Button>
-          </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
             </div>
           </div>
         </div>
-        {/* <Select>
+      </div>
+      {/* <Select>
         <Label>Favorite Animal</Label>
         <Button>
           <SelectValue />
@@ -95,7 +59,6 @@ export default function Index() {
           </ListBox>
         </Popover>
       </Select> */}
-      </div>
-    </>
+    </div>
   );
 }
